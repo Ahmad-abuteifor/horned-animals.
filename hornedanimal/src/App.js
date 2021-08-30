b82a6e2429de76130a491420eaf58ca181115ff1
Ahmad-abuteifor <ana1ahmad10@gmail.com>
2021-08-30 16:09:55 +0300

    finish lab 02

--- hornedanimal/src/App.js
+++ hornedanimal/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import logo from './logo.svg';
-
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Main from './components/main';
 import Header from './components/headr';
--- hornedanimal/src/components/HornedBeasts.js
+++ hornedanimal/src/components/HornedBeasts.js
@@ -1,22 +1,66 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 
 
 
 class HornedBeasts extends React.Component {
+
+    constructor(props) {
+
+        super(props)
+        this.state = {
+            votes: 0,
+          }
+        }
+
+        addvotes = () => {
+        
+            this.setState({
+                votes: this.state.votes + 1
+            });
+        
+        
+          }
+        
+
+        
+
     render(){
 return(
-    <div>
-    <h2>{this.props.title}</h2>
-    <p>
-    {this.props.detales}
-    </p>
-    <img src={this.props.imgUrl} alt={this.props.title}/>
+    // <div>
+    // <h2>{this.props.title}</h2>
+    // <p>
+    // {this.props.detales}
+    // </p>
+    // <img src={this.props.imgUrl} alt={this.props.title}/>
 
 
-    </div>
+    // </div>
 
+
+
+
+
+
+
+<div>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.props.imgUrl} />
+  <Card.Body>
+    <Card.Title> {this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.detales} ,<br></br>
+
+    number of votes    {this.state.votes}
+    </Card.Text>
+    <Button onClick={this.addvotes}> vote to  {this.props.title} </Button>
+  </Card.Body>
+</Card>
+</div>
 )
 
     }
